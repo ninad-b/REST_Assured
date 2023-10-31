c6fec26fc8b39f80254b78a005deeeca021cb032
ninad <ninad@Ninad-Legion>
2023-10-31 09:22:33 +0530

    Added a new test

---
+++ RestAutomation/src/test/java/restTests/NavigateToGitTest.java
@@ -0,0 +1,12 @@
+package restTests;
+
+import org.testng.annotations.Test;
+
+public class NavigateToGitTest {
+
+	@Test
+	public void navigateToGit()
+	{
+		System.out.println("We have navigated to GIT successfully");
+	}
+}
